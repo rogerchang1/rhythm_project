0b233baa82e26aea797f714f927b761383a9dd76
Roger! <mahjong.mahjong@gmail.com>
2020-02-23 13:19:25 -0800

    Moved functions from SongManager to BeatMapParser
    -BeatMapParser should handle parsing from the external beat map file and SongManager can receive the processed data

--- Assets/Scripts/BeatMapParser.cs
+++ Assets/Scripts/BeatMapParser.cs
@@ -6,15 +6,15 @@ using System;
 
 public class BeatMapParser : MonoBehaviour
 {
+    //External text file beat map
     public TextAsset beatMap;
     public float[][][] notes;
     public List<List<List<float>>> notesList;
 
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        generateNoteArray();
+        parseBeatMapToArray();
     }
 
     // Update is called once per frame
@@ -22,11 +22,13 @@ public class BeatMapParser : MonoBehaviour
     {
     }
 
-    void generateNoteArray()
+    //Parse TextAsset BeatMap (external text file) to a float[][][] array for holding notes data.
+    void parseBeatMapToArray()
     {
         //beatMap = Resources.Load("BeatMaps/beatMap1.txt") as TextAsset;
         string textMapString = beatMap.text.Trim();
         string[] textMapArr = textMapString.Split('\n');
+        int[] laneIdxArr;
 
         if (textMapArr.Length > 0)
         {
@@ -35,69 +37,82 @@ public class BeatMapParser : MonoBehaviour
             if (Int32.TryParse(textMapArr[0].Trim(), out numLanes))
             {
                 notes = new float[numLanes][][];
-                notesList = new List<List<List<float>>>();
+                laneIdxArr = new int[numLanes];
+                //notesList = new List<List<List<float>>>();
 
                 //creating each lane;
                 for (int i = 0; i < numLanes; i++)
                 {
                     notes[i] = new float[textMapArr.Length - 1][];
-                    notesList.Add(new List<List<float>>());
+                    // notesList.Add(new List<List<float>>());
                 }
 
                 //Construct notes array
-                int idx = 0;
                 for (int i = 1; i < textMapArr.Length; i++)
                 {
                     string[] beatLine = textMapArr[i].Split('/');
 
                     //Each line in the file needs to have the beat position as the first value. The values after the beat position represent the note type. Delimiter is '/'.
                     //The total number of values in the text file line should equal to numLanes + 1.
-                    if (beatLine.Length == numLanes + 1)
+                    if (beatLine.Length >= numLanes + 1)
                     {
                         float beat;
                         if (float.TryParse(beatLine[0].Trim(), out beat))
                         {
-                            
-                            for (int j = 1; j < beatLine.Length; j++)
+
+                            for (int j = 1; j < numLanes + 1; j++)
                             {
                                 float noteType = 0;
                                 float.TryParse(beatLine[j].Trim(), out noteType);
-                                Debug.Log(beat+","+ noteType);
-                                notes[j - 1][idx] = new float[] { beat, noteType };
-                                List<float> l = new List<float>() { beat, noteType };
-                                notesList[j - 1].Add(l);
+                                if (noteType > 0)
+                                {
+                                    notes[j - 1][laneIdxArr[j - 1]] = new float[] { beat, noteType };
+                                    List<float> l = new List<float>() { beat, noteType };
+                                    //notesList[j - 1].Add(l);
+                                    laneIdxArr[j - 1]++;
+                                }
+                                else
+                                {
+                                    //Invalid note, decrease array size for the lane by 1.
+                                    Array.Resize(ref notes[j - 1], notes[j - 1].Length - 1);
+                                }
                             }
-                            idx++;
+                        }
+                        else
+                        {
+                            //Invalid line in text file, decrease array size for all lanes by 1
+                            for (int j = 0; j < numLanes; j++)
+                            {
+                                Array.Resize(ref notes[j], notes[j].Length - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Invalid line in text file, decrease array size for all lanes by 1
+                        for (int j = 0; j < numLanes; j++)
+                        {
+                            Array.Resize(ref notes[j], notes[j].Length - 1);
                         }
                     }
                 }
 
                 for (int i = 0; i < notes.Length; i++)
                 {
-                    // notes[i] = sortLane(notes[i]);
+                    Array.Sort(notes[i], (a, b) => compareForSort(a, b));
                 }
             }
 
         }
     }
 
-    private float[][] sortLane(float[][] lane)
+    private int compareForSort(float[] a, float[] b)
     {
-        float[] beats = new float[lane[0].Length];
-        float[] noteTypes = new float[lane[0].Length];
-        float[][] output;
-        for(int i = 0; i < lane.Length; i++)
+        if (a[0] != b[0])
         {
-            beats[i] = lane[i][0];
-            noteTypes[i] = lane[i][1];
+            return a[0].CompareTo(b[0]) > 0 ? 1 : -1;
         }
-        Array.Sort(beats, noteTypes);
-        output = new float[beats.Length][];
-        for (int i = 0; i < beats.Length; i++)
-        {
-            output[i] = new float[] { beats[i], noteTypes[i] };
-        }
-        return output;
+        return 0;
     }
 
 }
--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -41,30 +41,12 @@ public class SongManager : MonoBehaviour
     //judgement bar positions
     private float judgementBar_width, judgementBar_xPos_left, judgementBar_yPos;
 
-    //External file beat map
-    public TextAsset beatMap;
-
     public KeyCode speedUp, speedDown;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        //string[] lane1 = new string[] { "20","24", "28", "32", "36", "40", "44", "48", "52", "56", "60", "64", "68", "72", "76", "80", "84", "88", "92", "96", "100" };//kick
-        //string[] lane2 = new string[] { "18", "19", "19.5", "22", "26", "30", "38", "42", "46", "50", "51", "51.5", "54", "58", "62", "66", "70", "74", "78", "82", "86", "90", "94", "98" }; //snare
-        string[] lane1 = new string[] { "20","24", "28", "32", "40", "57", "58", "62", "66", "66.5", "67", "67.5","70","79","86","91","93" };
-        string[] lane2 = new string[] { "18", "19", "19.5", "22", "26", "30", "46", "50", "51", "51.5", "54", "59", "61", "63", "68+69","72","74","78","80+83","84+85","88","90","94","96" }; //snare
-        string[] lane3 = new string[] { "47", "60", "75","77","95" };
-        string[] lane4 = new string[] { "43", "44", "70","79","86","95" };
-        string[] lane5 = new string[] {  "36","38","41","42", "68+69","71","73","74","78", "80+83", "84+85","87","89","90","94","96" };
-        string[] lane6 = new string[] { "34","34.5", "35","35.5","39", "45","48+49","52", "55+56",  "64+65", "75", "76","77","92","93" };
-        string[] emptyLane = new string[] {};
-        string[] testLane = new string[] { "12+16", "24"};
-
-        //notes = new float[][][] { noteParser(testLane), noteParser(emptyLane), noteParser(emptyLane), noteParser(emptyLane)};
-        //notes = new float[][][] { noteParser(lane1), noteParser(lane2), noteParser(lane3),noteParser(lane4),noteParser(lane5),noteParser(lane6)};
-        //notes = transform.GetComponent<BeatMapParser>().notes;
-        parseBeatMapToArray();
+        notes = transform.GetComponent<BeatMapParser>().notes;
         nextIndexArr = new int[notes.Length];
         lanes = new GameObject[notes.Length];
         judgementBar_width = judgementBar.GetComponent<SpriteRenderer>().bounds.size.x;
@@ -191,122 +173,4 @@ public class SongManager : MonoBehaviour
             }
         }
     }
-
-    //turn a string array of note beats into float[][]. Just to make it easier to input and design note charts....
-    private float[][] noteParser(string[] input)
-    {
-        float[][] output = new float[input.Length][];
-        for(int i = 0; i < input.Length; i++)
-        {
-            string[] inputStr = input[i].Split('+');
-            float[] temp = new float[inputStr.Length];
-            for(int j = 0; j<inputStr.Length;j++)
-            {
-                temp[j] = float.Parse(inputStr[j]);
-            }
-            output[i] = temp;
-        }
-        return output;
-    }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(2);
-    }
-
-    //Parse TextAsset BeatMap (external text file) to a float[][][] array for holding notes data.
-    void parseBeatMapToArray()
-    {
-        //beatMap = Resources.Load("BeatMaps/beatMap1.txt") as TextAsset;
-        string textMapString = beatMap.text.Trim();
-        string[] textMapArr = textMapString.Split('\n');
-        int[] laneIdxArr;
-
-        if (textMapArr.Length > 0)
-        {
-            //First value in the Beat Map must be the number of lanes.
-            int numLanes;
-            if (Int32.TryParse(textMapArr[0].Trim(), out numLanes))
-            {
-                notes = new float[numLanes][][];
-                laneIdxArr = new int[numLanes];
-                //notesList = new List<List<List<float>>>();
-
-                //creating each lane;
-                for (int i = 0; i < numLanes; i++)
-                {
-                    notes[i] = new float[textMapArr.Length - 1][];
-                   // notesList.Add(new List<List<float>>());
-                }
-
-                //Construct notes array
-                int idx = 0;
-                for (int i = 1; i < textMapArr.Length; i++)
-                {
-                    string[] beatLine = textMapArr[i].Split('/');
-
-                    //Each line in the file needs to have the beat position as the first value. The values after the beat position represent the note type. Delimiter is '/'.
-                    //The total number of values in the text file line should equal to numLanes + 1.
-                    if (beatLine.Length >= numLanes + 1)
-                    {
-                        float beat;
-                        if (float.TryParse(beatLine[0].Trim(), out beat))
-                        {
-
-                            for (int j = 1; j < numLanes + 1; j++)
-                            {
-                                float noteType = 0;
-                                float.TryParse(beatLine[j].Trim(), out noteType);
-                                if (noteType > 0)
-                                {
-                                    notes[j - 1][laneIdxArr[j - 1]] = new float[] { beat, noteType };
-                                    List<float> l = new List<float>() { beat, noteType };
-                                    //notesList[j - 1].Add(l);
-                                    laneIdxArr[j - 1]++;
-                                }
-                                else
-                                {
-                                    //Invalid note, decrease array size for the lane by 1.
-                                    Array.Resize(ref notes[j - 1], notes[j - 1].Length - 1);
-                                }
-                            }
-                            //idx++;
-                        }
-                        else
-                        {
-                            //Invalid line in text file, decrease array size for all lanes by 1
-                            for (int j = 0; j < numLanes; j++)
-                            {
-                                Array.Resize(ref notes[j], notes[j].Length - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        //Invalid line in text file, decrease array size for all lanes by 1
-                        for (int j = 0; j < numLanes; j++)
-                        {
-                            Array.Resize(ref notes[j], notes[j].Length - 1);
-                        }
-                    }
-                }
-
-                for (int i = 0; i < notes.Length; i++)
-                {
-                    Array.Sort(notes[i], (a, b) => compareForSort(a, b));
-                }
-            }
-
-        }
-    }
-
-    private int compareForSort(float[] a, float[] b)
-    {
-        if (a[0] != b[0])
-        {
-            return a[0].CompareTo(b[0]) > 0 ? 1 : -1;
-        }
-        return 0;
-    }
-
 }
