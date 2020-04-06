7beb499f313f914e20c84338476c91844ef8f918
Roger! <mahjong.mahjong@gmail.com>
2020-04-05 22:58:39 -0700

    Update DialogueManager
    Update DialogueManager
    Add Dialogue Animation

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class DialogueManager : MonoBehaviour
 {
 
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
     private Queue<string> sentences;
-    
+    private bool inDialogue;
+    public KeyCode keyToPress;
+
+    public Animator animator;   
     // Start is called before the first frame update
     void Start()
     {
         sentences = new Queue<string>();
+        inDialogue = false;
+    }
+
+    private void Update()
+    {
+        if (inDialogue)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DisplayNextSentence();
+            }
+        }
     }
 
     public void StartDialogue(Dialogue dialogue) {
         sentences.Clear();
+        inDialogue = true;
+        animator.SetBool("isOpen", inDialogue);
+        nameText.text = dialogue.name;
         foreach(string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
@@ -31,11 +52,23 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
     }
 
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char c in sentence.ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return null;
+        }
+    }
     void EndDialogue()
     {
-        Debug.Log("End");
+        inDialogue = false;
+        animator.SetBool("isOpen", inDialogue);
     }
 }
